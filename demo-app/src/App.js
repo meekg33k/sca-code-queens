b70608c8c1e77bc95390c6a8c4a9207940837fae
kerry-okpere <kerryokpere@gmail.com>
2020-01-10 17:28:30 -0500

    feat: create a component

--- demo-app/src/App.js
+++ demo-app/src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    message: "",
+  }
+  greeting = (e) =>{
+    e.preventDefault();
+    this.setState({
+      message: "hey there!!"
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <button onClick = {this.greeting}>greet</button>
+        <p>{this.state.message}</p>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
