7082fe81b71d7310d57bcb0f97750b4c3eaefd9c
kerry-okpere <kerryokpere@gmail.com>
2020-02-10 22:13:33 +0100

    Add header component tp app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 import Routes from './routes';
 
+import Header from './components/Header/index'
+
 const App = () => {
   return (
+    <div className='app'>
     <Routes />
+    <Header heading='This is the he' size={3}/>
+    </div>
   );
 }
 
