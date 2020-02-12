984a58ce06c216382fa5a48538bd6e714f972cca
kerry-okpere <kerryokpere@gmail.com>
2020-02-12 14:12:43 +0100

    update: Header snapshot test

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -3,6 +3,40 @@ import { shallow } from 'enzyme';
 
 import Header from "./index";
 
+describe('Header snapshot for different level heading', () => {
+    it('should match the H1 snapshot', () => {
+        const wrapper = shallow(
+            <Header 
+                heading= 'h1 heading'
+                color = "blue"
+            />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+    
+    it('should match the H2 snapshot', () => {
+        const wrapper = shallow(
+            <Header 
+                heading= 'h2 heading'
+                color = "blue"
+                size = {2}
+            />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+    it('should match the H3 snapshot', () => {
+        const wrapper = shallow(
+            <Header 
+                heading= 'h3 heading'
+                color = "blue"
+                size = {3}
+            />
+        );
+        expect(wrapper).toMatchSnapshot();
+    });
+    
+})
+
 describe('Header output', () =>{
 
     it('renders the title in <h1> tags', () => {
