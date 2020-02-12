991b89d53ebc201132af108a59543fa6f17ed779
kerry-okpere <kerryokpere@gmail.com>
2020-02-12 14:08:22 +0100

    update: test  Header output

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -3,11 +3,29 @@ import { shallow } from 'enzyme';
 
 import Header from "./index";
 
-describe('Header component', () =>{
+describe('Header output', () =>{
 
-    it('renders a heading element', () =>{
-        const header = shallow(<Header heading='try this to see'/>)
-        expect(header.text()).toBe('try this to see')
-    })
+    it('renders the title in <h1> tags', () => {
+        const header = shallow(<Header heading='show some heading' />);
+        const title = <h1>show some heading</h1>;
     
+        expect(header.contains(title)).toEqual(true);
+    });
+    it('renders the title in <h2> tags', () => {
+        const header = shallow(<Header heading='show some heading' size={2}/>);
+        const title = <h2>show some heading</h2>;
+    
+        expect(header.contains(title)).toEqual(true);
+    });
+    it('renders the title in <h3> tags', () => {
+        const header = shallow(<Header heading='show some heading' size={3}/>);
+        const title = <h3>show some heading</h3>;
+    
+        expect(header.contains(title)).toEqual(true);
+    });
+    // it('renders a heading element', () =>{
+    //     const header = shallow(<Header heading='try this to see'/>)
+    //     expect(header.text()).toBe('try this to see')
+    // })    
 })
+
