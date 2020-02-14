ea76437306e4ba7849a03445477c613b8eaf8955
AnthoniaOkafor <tonchuks@gmail.com>
2020-02-14 11:28:55 +0100

    created Text component, imported it to App component and tested it

---
+++ src/components/Text/Text.test.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Text from './index'
+import {shallow} from 'enzyme'
+
+describe ('Text Component render', () => {
+    //it('renders correctly', () => {
+        //const wrapper = shallow(<Text text='It is a text' />);
+        //expect(wrapper).toMatchSnapShot();
+    //});
+
+    it('displays text', () => {
+        const wrapper = shallow(<Text text='It is a text' />);
+        expect(wrapper.text()).toBe('It is a text');
+    });
+});
