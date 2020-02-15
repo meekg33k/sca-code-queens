3e046fc1f1c513fa89fa67de95499731a86fb8ae
kerry-okpere <kerryokpere@gmail.com>
2020-02-15 20:58:00 +0100

    Update: test output text  for Header component

--- src/components/Header/Header.test.js
+++ src/components/Header/Header.test.js
@@ -36,30 +36,18 @@ describe('Header snapshot for different level heading', () => {
     });
     
 })
+    let title;
+
+    beforeEach(() => {
+        title = 'show some heading'
+    })
 
 describe('Header output', () =>{
 
     it('renders the title in <h1> tags', () => {
-        const header = shallow(<Header heading='show some heading' />);
-        const title = <h1>show some heading</h1>;
-    
-        expect(header.contains(title)).toEqual(true);
+        const header = shallow(<Header heading= {title} />);
+        expect(header.text()).toEqual(title);
     });
-    it('renders the title in <h2> tags', () => {
-        const header = shallow(<Header heading='show some heading' size={2}/>);
-        const title = <h2>show some heading</h2>;
     
-        expect(header.contains(title)).toEqual(true);
-    });
-    it('renders the title in <h3> tags', () => {
-        const header = shallow(<Header heading='show some heading' size={3}/>);
-        const title = <h3>show some heading</h3>;
-    
-        expect(header.contains(title)).toEqual(true);
-    });
-    // it('renders a heading element', () =>{
-    //     const header = shallow(<Header heading='try this to see'/>)
-    //     expect(header.text()).toBe('try this to see')
-    // })    
 })
 
