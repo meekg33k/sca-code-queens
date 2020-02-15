937755064562eef8ac46397c8f85af872371115d
kerry-okpere <kerryokpere@gmail.com>
2020-02-15 20:59:26 +0100

    Update: returns semantic element

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,21 +3,15 @@ import React from "react";
 const Header = ({heading, color, size}) => {
     if (size === 3) {
         return(
-            <div className="header">
-                <h3 color={color}>{heading}</h3>
-            </div>
+            <h3 className='heading' color={color}>{heading}</h3>
         )
     }else if (size === 2) {
         return(
-            <div className="header">
-                <h2 color={color}>{heading}</h2>
-            </div>
+            <h2 className='heading' color={color}>{heading}</h2>
         )
     }else 
         return(
-            <div className="header">
-                <h1 color={color}>{heading}</h1>
-            </div>
+            <h1 className='heading' color={color}>{heading}</h1>
         )
 }
 
