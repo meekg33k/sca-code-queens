f7e3c06e0cef289346501fd3bead471de406684d
kerry-okpere <kerryokpere@gmail.com>
2020-02-25 14:18:00 +0100

    Update tests to reflect refactored changes

--- src/components/ListOfLink/ListOfLink.test.js
+++ src/components/ListOfLink/ListOfLink.test.js
@@ -1,27 +1,43 @@
 import React from "react";
 import { shallow } from 'enzyme';
+import { socials } from "../Footer/";
 
 import ListOfLink from "./"
 
-let setUp = (prop) =>{
-    return shallow(<List links={prop}/>)
+let icon = socials
+let link = ["Blogs", "Company"]
+
+let setUp = (prop ={}) =>{
+    return shallow(<ListOfLink item={prop}/>)
 }
 
 describe('List Component', () =>{
 
-    let link = ["link", "kerry"];
-    let list
-    beforeEach(()=>{ 
-        list = setUp(link)
-    })
-
     it('should match Icon snap shot', () =>{
-        expect(list).toMatchSnapshot();
-    })
-    it('should render the right amount of links on list', ()=>{
-        const nodes = list.find('li');
-
-        expect(nodes.length).toBe(link.length)
+        const listSnap = setUp(icon)
+        expect(listSnap).toMatchSnapshot();
     })
   
+})
+describe('renders a list of icons', ()=> {
+    let listOfIcon
+     beforeEach(()=>{ 
+        listOfIcon = setUp(icon)
+    })
+    it('and find the Icon node', ()=>{
+        const icon = listOfIcon.find('Icon');
+
+        expect(icon.length).toBe(3)
+    })
+})
+describe('renders a list of link', ()=> {
+    let listOfLink
+    beforeEach(()=>{ 
+        listOfLink = setUp(link)
+    })
+    it('and find the list of Links node', ()=>{
+        const list = listOfLink.find('Link');
+
+        expect(list.length).toBe(2)
+    })
 })
