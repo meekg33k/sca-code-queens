7ee6a07c35f63aa671eb2f0de2150489c1f28274
kerry-okpere <kerryokpere@gmail.com>
2020-03-05 21:12:43 +0100

    Merge conflict resolved

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 
 import Footer from "../../components/Footer"
@@ -11,17 +10,5 @@ export const LandingPage = () => {
 		
 	)
 }
-=======
-import React from "react";
-import Header from "../../components/Header/index";
-
-export const LandingPage = () => {
-  return (
-    <div className="landingPage">
-      <Header heading="This is the landing page" color="green" />
-    </div>
-  );
-};
->>>>>>> 1b17d35f97a3b3f6bf2704d1389b8f079bde352e
 
 export default LandingPage;
