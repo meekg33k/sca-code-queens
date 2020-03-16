0f4794ce8cf85da329f79bd75fd123a938b81699
kerry-okpere <kerryokpere@gmail.com>
2020-03-16 22:29:21 +0100

    Add search box to Jumbotron

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
-
+import Jumbotron from '../../components/Jumbotron'
 import Footer from "../../components/Footer"
 
 export const LandingPage = () => {
+	
 	return (
 		<div className='landingPage'>
+      <Jumbotron />
 			<Footer/>
 		</div>
 		
 	)
 }
+
 
 export default LandingPage;
