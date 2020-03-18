d71177662dda94bd37d0ad99cc8c2d14a347c10e
kerry-okpere <kerryokpere@gmail.com>
2020-03-18 14:44:37 +0100

    make searchBox dynamic

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -4,18 +4,24 @@ import "./SearchBox.css"
 
 import Button from "../Button"
 
-const SearchBox = ({onSearch}) =>{
+const SearchBox = ({onSearch, buttonTitle}) =>{
 
 	const handleSubmit = (e) =>{
 		e.preventDefault()
-		const searchInput = document.getElementById("box");
+		const searchInput = document.getElementById("box")	;
 		onSearch(searchInput.value);
 		searchInput.value = "";
-}
+	}
+	let button = ()=>{
+		if(buttonTitle){
+			return <Button title={buttonTitle} onClick={handleSubmit}/>
+		}
+	}
 	return(
 		<form className="search" onSubmit={handleSubmit}>
-			<input className="searchBox" id="box" type='text' placeholder='&#xf002; what event are you searching for?'/>
-			<Button title={"search"} onClick={handleSubmit}/>
+			<input className="searchBox" id="box" type='text' placeholder='&#xf002; what event are you searching for &#x3f;'/>
+			{/* could be better */}
+			{button()}
 		</form>
 	)
 }
