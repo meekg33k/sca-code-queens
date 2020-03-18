e845d9fb4a714a8114099f0ca5e16fb04d311644
Uduak 'Eren <sandy3jnr@gmail.com>
2020-03-18 14:53:00 -0700

    Refactored Navigation to be more reusable, updated tests

--- src/components/Navigation/Navigation.test.js
+++ src/components/Navigation/Navigation.test.js
@@ -1,13 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { mount } from "enzyme";
-
 import Navigation from "./index";
-//import { render } from "@testing-library/react";
 
 let nav;
 const setUp = () => {
-  return mount (
+  return mount(
     <Router>
       <Navigation />
     </Router>
@@ -18,19 +16,36 @@ beforeEach(() => {
 });
 
 describe("Navigation Component render", () => {
-    it("renders corectly", () => {
-      expect(nav).toMatchSnapshot();
-    });
-    it ("renders links through the NavLink node", () => {
-      const findNavLink = nav.find('NavLink');
-      expect(findNavLink.length).toBe(4);
-    });
-    it ("renders a list of menu", () => {
-      const findMenu = nav.find("li");
-      expect(findMenu.at(0).text()).toBe("Home");
-      expect(findMenu.at(1).text()).toBe("Organise");
-      expect(findMenu.at(2).text()).toBe("How it works");
-      expect(findMenu.at(3).text()).toBe("Sign in");
+  it("renders corectly", () => {
+    expect(nav).toMatchSnapshot();
+  });
+  it("renders links through the NavLink node", () => {
+    const findNavLink = nav.find("NavLink");
+    expect(findNavLink.length).toBe(4);
+  });
+  it("renders the default list of menu", () => {
+    const findMenu = nav.find("li");
+    expect(findMenu.at(0).text()).toBe("Home");
+    expect(findMenu.at(1).text()).toBe("Organise");
+    expect(findMenu.at(2).text()).toBe("How it works");
+    expect(findMenu.at(3).text()).toBe("Sign in");
+  });
 
-    });
-})
+  it("renders navItems props as menu list", () => {
+    const navItems = [
+      { name: "Link A", url: "/linka" },
+      { name: "Link B", url: "/linkb" },
+      { name: "Link C", url: "/linkc" }
+    ];
+    const dynamicNav = mount(
+      <Router>
+        <Navigation navItems={navItems} />
+      </Router>
+    );
+    expect(dynamicNav.find("NavLink").length).toBe(3);
+    const findMenu = dynamicNav.find("li");
+    expect(findMenu.at(0).text()).toBe("Link A");
+    expect(findMenu.at(1).text()).toBe("Link B");
+    expect(findMenu.at(2).text()).toBe("Link C");
+  });
+});
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,28 +1,40 @@
 import React from "react";
-//import PropTypes from "prop-types";
-import './Navigation.css';
+import PropTypes from "prop-types";
+import "./Navigation.css";
 import { NavLink } from "react-router-dom";
 
+const items = [
+  { name: "Home", url: "/home" },
+  { name: "Organise", url: "/organise" },
+  { name: "How it works", url: "/howitworks" },
+  { name: "Sign in", url: "./signin" }
+];
 
+const Navigation = ({ navItems }) => {
+  return (
+    <header className="header">
+      <ul>
+        {navItems.map(menuItem => {
+          return (
+            <li className="navList" key={menuItem.name}>
+              <NavLink to={menuItem.url}>{menuItem.name}</NavLink>
+            </li>
+          );
+        })}
+      </ul>
+    </header>
+  );
+};
 
-const Navigation = ({navname}) => {
-    const navItems = [{name:"Home", url:"/home"}, {name:"Organise", url:"/organise"}, {name:"How it works", url:"/howitworks"}, {name:"Sign in", url:"./signin"}]
-    
-    return (
-        <header className="header">
-            <ul>
-             {navItems.map(menuItem => {
-                 return <li className="navList" key={menuItem.name}>
-                 {<NavLink to={menuItem.url}>{menuItem.name}</NavLink>}
-                 </li>;
-             }
-            )}
-            </ul>
+Navigation.defaultProps = { navItems: items };
 
-        </header>
-    );
-}
+Navigation.propTypes = {
+  navItems: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      url: PropTypes.string
+    })
+  )
+};
 
-
-
-export default Navigation
+export default Navigation;
