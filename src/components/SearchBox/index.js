d505565c4cbfd153fd85379504635c6391ff999c
kerry-okpere <kerryokpere@gmail.com>
2020-03-19 10:12:14 +0100

    Update the dynamic rendering of button

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -12,16 +12,11 @@ const SearchBox = ({onSearch, buttonTitle}) =>{
 		onSearch(searchInput.value);
 		searchInput.value = "";
 	}
-	let button = ()=>{
-		if(buttonTitle){
-			return <Button title={buttonTitle} onClick={handleSubmit}/>
-		}
-	}
+	
 	return(
 		<form className="search" onSubmit={handleSubmit}>
 			<input className="searchBox" id="box" type='text' placeholder='&#xf002; what event are you searching for &#x3f;'/>
-			{/* could be better */}
-			{button()}
+			{buttonTitle && <Button title={buttonTitle} onClick={handleSubmit} />}
 		</form>
 	)
 }
