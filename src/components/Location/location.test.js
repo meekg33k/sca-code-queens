dc8c671a810d4436e890f30d12a6128a47695d1b
AnthoniaOkafor <tonchuks@gmail.com>
2020-03-20 21:38:53 +0100

    deleted unneeded file and updated test

--- src/components/Location/location.test.js
+++ src/components/Location/location.test.js
@@ -20,4 +20,18 @@ describe ("Location Component render", () => {
             const faIcon = wrapper.find("i");
             expect(faIcon.length).toBe(1);
      })
+
+     it("renders text on the page", () => {
+        const wrapper = shallow(<Location />);
+            const findText = wrapper.find("p");
+            expect(findText.length).toBe(2)
+     })
+
+     it("renders correct text", () => {
+      const wrapper = shallow(<Location name="The name of the event" location="The location of the event" />);
+      expect(wrapper.text()).toBe("The name of the eventThe location of the event");
+      console.log(wrapper.debug());
+     })
+    
+      
 });
