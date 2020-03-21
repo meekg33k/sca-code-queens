ff0ff82f6f7713996d90d36fd863c37791e32f50
kerry-okpere <kerryokpere@gmail.com>
2020-03-21 08:29:33 +0100

    Add event page to route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import LandingPage from "../pages/LandingPage/index";
+import EventPage from "../pages/EventPage/index";
+
 
 const Routes = () => (
   <Router>
     <Switch>
       <Route exact path="/" component={LandingPage} />
+      <Route  path="/event" component={EventPage} />
     </Switch>
   </Router>
 );
