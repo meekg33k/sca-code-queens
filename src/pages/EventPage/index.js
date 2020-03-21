ed7b15788bcc35141f51238788893c3701fba1cf
kerry-okpere <kerryokpere@gmail.com>
2020-03-21 08:29:03 +0100

    Add component to event page

--- src/pages/EventPage/index.js
+++ src/pages/EventPage/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Footer from "../../components/Footer";
 import Navigation from "../../components/Navigation";
+import EventDetail from "../../components/EventDetail";
+
 
 export const EventPage = () => {
   return (
     <div className="event-page">
       <Navigation />
+      <EventDetail 
+      about={"DJ Spinal shutting Down popular club in Yaba"}
+      time={"8:00 PM - Dawn"}
+      date={"28th January, 2020"}/>
       <Footer />
+      
     </div>
   );
 };
