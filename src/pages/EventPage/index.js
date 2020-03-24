f4662f43ad565b22721596941609d121ab8ec3bf
AnthoniaOkafor <tonchuks@gmail.com>
2020-03-24 22:13:22 +0100

    created CoverPhoto component, adjusted EventDetails component, made both component responsive

--- src/pages/EventPage/index.js
+++ src/pages/EventPage/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import Footer from "../../components/Footer";
 import Navigation from "../../components/Navigation";
 import EventDetail from "../../components/EventDetail";
-import Song from "../../components/Song";
+import CoverPhoto from "../../components/CoverPhoto";
 import Location from "../../components/Location";
+import "./EventPage.css";
 
 export const EventPage = () => {
   return (
@@ -13,11 +14,17 @@ export const EventPage = () => {
         name= "DJ Spinal Take Over" 
         location="Party Club Estate, Alalaba, Lagos" 
       />
-      <Song />
-      <EventDetail 
-      about={"DJ Spinal shutting Down popular club in Yaba"}
-      time={"8:00 PM - Dawn"}
-      date={"28th January, 2020"}/>
+      <div className="songDetails">
+        <div className="photoCover">
+            <CoverPhoto name="DJ Spinal" coverimg="./assets/smiley.jpg" />
+        </div>
+        <div className="eventDetail">
+            <EventDetail 
+            about={"DJ Spinal shutting Down popular club in Yaba"}
+            time={"8:00 PM - Dawn"}
+            date={"28th January, 2020"}/>
+        </div>
+      </div>
       
       <Footer />
       
