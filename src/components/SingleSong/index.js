454a899c02e000edfc6e0d2de918bbe8f1617ebe
AnthoniaOkafor <tonchuks@gmail.com>
2020-04-08 01:38:19 +0100

    worked on styling

--- src/components/SingleSong/index.js
+++ src/components/SingleSong/index.js
@@ -39,30 +39,48 @@ class SingleSong extends React.Component {
             this.setState({isPlaying: false})
         }
 
-        let start = (this.state.time === 0 || this.state.isPlaying === false) ? 
-        <button onClick={this.startTimer}>
+        let start = (this.state.time === 0 || this.state.isPlaying === false) ?
+        <button className="iconStop" onClick={this.startTimer}>
             <Icon src="./assets/play.png" alt="play button" />
+            <div className="playStop">
+                <Text text="Play" color="greyish" size="default"/>
+            </div>
         </button> : null
 
         let stop = (this.state.time !== 0 && this.state.isPlaying === true) ?
-        <button onClick={this.stopTimer}>
+        <button className="iconStop" onClick={this.stopTimer}>
             <Icon src="./assets/stop.png" alt="stop button" />
+            <div className="playStop">
+                <Text className="playStop" text="Stop" color="greyish" size="default"/>
+            </div>
         </button> : null
 
         return (
             <div className="addBorder">
                 <div className="singleSong">
-                    <Image source={this.props.albumPhoto} height = {100} width = {100} alt="Photo of this album"  />
-                    <div className="albumSinger">
-                        <div className="songSinger">
-                            <Text text="Song Singer - " color = "greyish" size="extrasmall" />
+                    <div className="partyImage">
+                        <Image source={this.props.albumPhoto} height = {100} width = {100} alt="Photo of this album"  />
+                    </div>    
+                    <div className="songTimer">
+                        <div className="albumSinger">
+                            <div className="songSinger">
+                                <Text text="Song Singer  - " color = "greyish" size="extrasmall" />
+                            </div>
+                            <div className ="singerName">
+                                <Text text= {this.props.singer} color= "greyish" size="extrasmall" />
+                            </div>
+                            <div className="timer">{this.state.time} {duration} </div>
                         </div>
-                        <div className ="singerName">
-                            <Text text= {this.props.singer} color= "greyish" size="extrasmall" />
+                    
+                        <div className="buttonAndIcon">
+                            <div className="buttonIcon"> {start} {stop} </div>
+                            <div className="likeOrUnlike">
+                                <LikeUnlike source="./assets/thumbsup.png" alternate="like" />
+                                <div className = "unlike">
+                                    <LikeUnlike source="./assets/thumbsdown.png" alternate="unlike" />
+                                </div>
+                            </div>
                         </div>
-                        <div>timer: {this.state.time} {duration} </div>
-                        <div className="buttonTimer"> {start} {stop} </div>
-                        
                     </div>
                 </div>
             </div>
